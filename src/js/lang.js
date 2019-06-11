3421b48b1f7ea33fc34e2d1a8644f7315ed5882e
huanghaojie <1107408003@qq.com>
2019-06-11 10:52:17 +0800

    i18n国际化

---
+++ src/js/lang.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "zh-cn": {
+        "RecentlyUsedFile": "我分享的文档",
+        "All": "全部",
+        "NoneData": "暂无文档",
+        "ErrorLoadData": "加载失败"
+    },
+    "zh-hk": {
+        "RecentlyUsedFile": "我分享的檔案",
+        "All": "全部",
+        "NoneData": "暫無檔案",
+        "ErrorLoadData": "加載失敗"
+    },
+    "en": {
+        "RecentlyUsedFile": "File i share",
+        "All": "All",
+        "NoneData": "No archives data",
+        "ErrorLoadData": "loading failed"
+    }
+}
