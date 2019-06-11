7d290e5209a0ab0614ddf9e9fdbc107e8066b8c4
huanghaojie <1107408003@qq.com>
2019-06-11 11:09:43 +0800

    i18n国际化

--- src/js/lang.js
+++ src/js/lang.js
@@ -1,18 +1,18 @@
 module.exports = {
     "zh-cn": {
-        "RecentlyUsedFile": "我分享的文档",
+        "FileIShare": "我分享的文档",
         "All": "全部",
         "NoneData": "暂无文档",
         "ErrorLoadData": "加载失败"
     },
     "zh-hk": {
-        "RecentlyUsedFile": "我分享的檔案",
+        "FileIShare": "我分享的檔案",
         "All": "全部",
         "NoneData": "暫無檔案",
         "ErrorLoadData": "加載失敗"
     },
     "en": {
-        "RecentlyUsedFile": "File i share",
+        "FileIShare": "File i share",
         "All": "All",
         "NoneData": "No archives data",
         "ErrorLoadData": "loading failed"
